2258ea1ecfb1afc1559ba1b496509f8cca21a413
akairat <kairat.ashim@gmail.com>
2015-11-26 18:40:23 -0500

    improved posting a project page. Added tags

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -2,11 +2,14 @@
 
 // Wrapped in an immediately invoked function expression.
 (function() {
-    // listener for posting a new project
-    $(document).on('submit', 'form#post-project', function(evt) {
+
+    ////////////// START: listeners for posting a new project /////////////////////
+    $(document).on('click', '#submit-project-post', function(evt) {
         evt.preventDefault();
         var title = $('#project-post-title').val();
         var description = $('#project-post-description').val();
+        var tags = $("input").tagsinput('items');
+        console.log(tags);
         var imageLinks = $('#project-post-image-links').val();
         $.post(
             '/projects',
@@ -21,6 +24,25 @@
             $('.error').text(response.err);
         });
     });
+
+    var max_image_links = 5;
+    var field_count = 1;
+    $(document).on('click', '.add_imagelink_field_button', function(event){
+        event.preventDefault();
+        if (field_count < max_image_links){
+            field_count++;
+            var images_wrapper = $('.image_input_fields_wrap');
+            images_wrapper.append('<div><input type="text" class="form-control project-post-image-links" placeholder="Enter project image link..."><button class="btn btn-warning btn-sm remove_imagelink_field">Remove</button></div>');
+        }
+    });
+
+    $(document).on('click', '.remove_imagelink_field', function(event){
+        event.preventDefault();
+        $(this).parent('div').remove();
+        field_count--;
+    });
+
+    ////////////// END: listeners for posting a new project /////////////////////
 
     $(document).on('click', '#myButton-upvote', function(evt) {
         evt.preventDefault();
